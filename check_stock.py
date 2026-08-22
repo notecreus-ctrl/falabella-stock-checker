80a2a34ddc686e870a294ecd28f72ccd7cacc0f1
notecreus-ctrl <notecreus@gmail.com>
2026-08-22 13:45:14 -0400

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -16,17 +16,11 @@ def check_search():
         "Referer": "https://www.falabella.com/"
     }
     r = requests.get(SEARCH_URL, headers=headers, timeout=TIMEOUT)
-
-    c1 = len(re.findall("falabella-cl/product/", r.text))
-    c2 = r.text.count('"@type":"Product"')
-    c3 = r.text.count("application/ld+json")
-    c4 = r.text.count("&quot;@type&quot;:&quot;Product&quot;")
-    c5 = r.text.count("&quot;url&quot;:&quot;https://www.falabella.com/falabella-cl/product/")
-
-    print("falabella-cl/product/: " + str(c1))
-    print('@type Product: ' + str(c2))
-    print("ld+json: " + str(c3))
-    print("&quot;@type&quot; Product: " + str(c4))
-    print("&quot;url&quot; product: " + str(c5))
+    matches = re.findall(r'falabella-cl/product/[^"&\s<>]+', r.text)
+    seen = set()
+    for m in matches:
+        if m not in seen:
+            seen.add(m)
+            print(m)
 
 check_search()
