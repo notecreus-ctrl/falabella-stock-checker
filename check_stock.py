20c7c7a3ee6cfff589b4f9b8e6546587ddfa263f
notecreus-ctrl <notecreus@gmail.com>
2026-08-22 13:35:26 -0400

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -16,14 +16,17 @@ def check_search():
         "Referer": "https://www.falabella.com/"
     }
     r = requests.get(SEARCH_URL, headers=headers, timeout=TIMEOUT)
-    
-    patterns = [
-        r'falabella-cl/product/\d+',
-        r'"@type":"Product"',
-        r'application/ld\+json',
-        r'&quot;@type&quot;:&quot;Product&quot;',
-        r'&quot;url&quot;:&quot;https://www\.falabella\.com/falabella-cl/product/',
-    ]
-    
-    for p in patterns:
-        count = len(re.findall(p
+
+    c1 = len(re.findall("falabella-cl/product/", r.text))
+    c2 = r.text.count('"@type":"Product"')
+    c3 = r.text.count("application/ld+json")
+    c4 = r.text.count("&quot;@type&quot;:&quot;Product&quot;")
+    c5 = r.text.count("&quot;url&quot;:&quot;https://www.falabella.com/falabella-cl/product/")
+
+    print("falabella-cl/product/: " + str(c1))
+    print('@type Product: ' + str(c2))
+    print("ld+json: " + str(c3))
+    print("&quot;@type&quot; Product: " + str(c4))
+    print("&quot;url&quot; product: " + str(c5))
+
+check_search()
